ba98de0229bad31bdc8cf56afa9eb0f376cb6ef1
awesomak <bomjara24@mail.ru>
2019-05-03 15:05:17 +0300

    aa

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const cors = require('cors');
 
 const app = express();
 
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
 // Bodyparser Middleware
 app.use(express.json());
 app.use(cors())
