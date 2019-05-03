3f194a34e2e266aca8c1a83aa08251999a6fd339
awesomak <bomjara24@mail.ru>
2019-05-03 15:15:26 +0300

    aa

--- server.js
+++ server.js
@@ -40,16 +40,6 @@ app.use('/api/home', require('./routes/api/home'));
 app.use('/api/client', require('./routes/api/client'));
 app.use('/api/comment', require('./routes/api/comment'));
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
